3114c0bfb1e7a4a3b3e68c3cb6538d71aedd7d78
Naveez Munir <naveezmunir@gmail.com>
2025-04-29 18:48:42 +0500

    added stream message fetaure in api end point

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -8,7 +8,28 @@ export async function POST(req: Request) {
   const result = await client.responses.create({
     model: "gpt-4-turbo",
     input,
+    stream: true,
   });
 
-  return Response.json(result.output_text);
+  const stream = new ReadableStream({
+    async start(controller) {
+      for await (const chunk of result) {
+        if (chunk.type === "response.output_text.delta" && chunk.delta) {
+          controller.enqueue(new TextEncoder().encode(chunk.delta));
+        }
+      }
+
+      controller.close();
+    },
+  });
+
+
+  return new Response(stream, {
+    headers: {
+      Connection: "keep-alive",
+      "Content-Encoding": "none",
+      "Cache-Control": "no-cache, no-transform",
+      "Content-Type": "text/event-stream; charset=utf-8",
+    },
+  });
 }
